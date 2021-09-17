98e1ea46e4fb6ee0c3ffab4029bb4cbff68b17e2
reags1110 <84357221+reags1110@users.noreply.github.com>
2021-09-16 19:20:49 -0500

    junit-studio finished

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,7 +10,53 @@ public class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
-        assertEquals(true, true);
+        assertTrue(true);
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void onlyLeftOneBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void onlyRightOneBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void stringBeforeBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("I Love you.[]"));
+    }
+    @Test
+    public void stringBehindBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]I Love you."));
+    }
+    @Test
+    public void stringMixBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("I Love you[Damian]."));
+    }
+    @Test
+    public void stringMixTwoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("I [Love] you[Damian]."));
+    }
+    @Test
+    public void Test1() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("I [Love you Damian."));
+    }
+    @Test
+    public void Test2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("I Love] you Damian."));
+    }
+    @Test
+    public void Test3() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("I ]Love] you Damian."));
+    }
+    @Test
+    public void Test4() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("I [Love[ you Damian."));
     }
 
 
